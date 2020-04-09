07059944d033dbce6482a86d8c9771ea5948c48f
ChinaraGuliyeva <chinarai@yandex.ru>
2020-04-10 00:41:34 +0400

    practically üorking version

--- script.js
+++ script.js
@@ -61,13 +61,44 @@ const sortDown = ()=> {
 let dragNdrop = () => {
     let draggables=document.querySelectorAll('.input-container');
     console.log(draggables);
-    draggables.forEach( draggable => {
+    draggables.forEach(draggable => {
         draggable.addEventListener('dragstart', () => {
-            console.log('drag start');
+            draggable.classList.add('dragging');
+        });
+        draggable.addEventListener('dragend', () => {
+            draggable.classList.remove('dragging');
         })
     })
 
-    parentContainer.addEventListener('dragover', () => {console.log('drag over')})
+    parentContainer.addEventListener('dragover', e => {
+        e.preventDefault();
+        const afterElement = getDragAfterElement(parentContainer, e.clientY);
+        console.log(afterElement);
+        const draggable = document.querySelector('.dragging');
+        if (afterElement==null) {
+            parentContainer.appendChild(draggable);
+        }
+       else {
+            parentContainer.insertBefore(draggable, afterElement)
+       }
+        })
+
+    let getDragAfterElement = (parentContainer, y) => {
+        const draggableElements = [...parentContainer.querySelectorAll('.input-container:not(.dragging)')];
+
+        return draggableElements.reduce((closest, child)=> {
+            const box = child.getBoundingClientRect();
+            const offset = y - box.top - box.height / 2;
+            console.log(offset);
+            if (offset < 0 && offset >closest.offset) {
+                return { offset: offset, element: child}
+            }
+            else {
+                return closest
+            }
+        },
+        { offset: Number.NEGATIVE_INFINITY}).element;
+    }
 }
 
 
