bb4d477de77339b7038eba60b1cb7cacfd11e0bb
ChinaraGuliyeva <chinarai@yandex.ru>
2020-04-09 23:01:20 +0400

    working on draggable

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const button = document.querySelector('.add-button');
-const deleteListener = document.querySelector('.parent-container');
+const parentContainer = document.querySelector('.parent-container');
 const arrowUp = document.querySelector('.sort-arrow-up');
 const arrowDown = document.querySelector('.sort-arrow-down');
+let inputContainer=document.querySelector('.input-container');
+let threeNods = document.querySelector('.drag');
+let deleteIcon = document.querySelector('.delete');
 
 const addItemHandler = (event) =>{
     let inputValue=document.querySelector('input');
-    let inputContainer=document.querySelector('.input-container');
     let newLine = inputContainer.cloneNode(true);
     let parent = document.querySelector('.parent-container');
     parent.appendChild(newLine);
 }
 
 const removeItem = (event) => {
-    let deleteButtons=document.querySelectorAll('.delete');
     let element=event.target;
     if(element.classList.contains("delete")){
         element.parentNode.remove();
@@ -25,7 +26,17 @@ const sortUp = ()=> {
     const inputs = document.querySelectorAll('input');
     let inputsArray = Array.from(inputs);
     inputsArray.sort((a, b) => {return a.value-b.value})
-    console.log(inputsArray);
+    let editedArray = [];
+    for (let i=0; i<inputsArray.length; i++) {
+        let result = inputContainer.cloneNode(true);
+        result.appendChild(inputsArray[i]);
+        editedArray.push(result);
+    }
+    //console.log(editedArray);
+    parentContainer.innerHTML="";
+    editedArray.forEach(element => {
+        parentContainer.appendChild(element);
+    });
 }
 
 const sortDown = ()=> {
@@ -34,10 +45,34 @@ const sortDown = ()=> {
     const inputs = document.querySelectorAll('input');
     let inputsArray = Array.from(inputs);
     inputsArray.sort((a, b) => {return b.value-a.value})
-    console.log(inputsArray);
+    let editedArray = [];
+    for (let i=0; i<inputsArray.length; i++) {
+        let result = inputContainer.cloneNode(true);
+        result.appendChild(inputsArray[i]);
+        editedArray.push(result);
+    }
+    //console.log(editedArray);
+    parentContainer.innerHTML="";
+    editedArray.forEach(element => {
+        parentContainer.appendChild(element);
+    });
 }
 
+let dragNdrop = () => {
+    let draggables=document.querySelectorAll('.input-container');
+    console.log(draggables);
+    draggables.forEach( draggable => {
+        draggable.addEventListener('dragstart', () => {
+            console.log('drag start');
+        })
+    })
+
+    parentContainer.addEventListener('dragover', () => {console.log('drag over')})
+}
+
+
 button.addEventListener('click', addItemHandler);
-deleteListener.addEventListener('click', removeItem);
+parentContainer.addEventListener('click', removeItem);
 arrowUp.addEventListener('click', sortUp);
 arrowDown.addEventListener('click', sortDown);
+button.addEventListener('click', dragNdrop);
